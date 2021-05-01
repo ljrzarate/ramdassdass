ebbadcf24c4613580c20e1740b41cdc78464f236
Luis Rodriguez <ljrzarate@gmail.com>
2021-04-30 19:56:50 -0500

    test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
+
     mail(to: @user.email, subject: 'Welcome to Ram Dass Dass, Hope you enjoy the readings!')
   end
 end
