6686ce14c6778c82745fe4758b3c0a7fc2298f8e
Luis Rodriguez <ljrzarate@gmail.com>
2022-10-11 22:44:40 -0500

    Fix pagination on admin side

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,1 +1,15 @@
-WillPaginate.per_page = 10
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        def per(value = nil) per_page(value) end
+        def total_count() count end
+        def first_page?() self == first end
+        def last_page?() self == last end
+      end
+    end
+    module CollectionMethods
+      alias_method :num_pages, :total_pages
+    end
+  end
+end
