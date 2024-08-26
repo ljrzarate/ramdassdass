d58c5655b930b0f23214618f575a24c6703182d4
Luis Rodriguez <ljrzarate@gmail.com>
2024-08-26 10:46:14 -0500

    change cuentos name in the sidebar and add tags to active admin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,16 +1,5 @@
 ActiveAdmin.register Post do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :content, :title, :summary
-  #
-  # or
-
-
-  filter :title
+ filter :title
 
   controller do
     def find_resource
@@ -43,6 +32,10 @@ ActiveAdmin.register Post do
         f.input :content, as: :froala_editor, input_html: { data: { options: { imageUploadParam: 'file_upload', imageUploadURL: upload_admin_post_path(resource.id), toolbarButtons: %w[undo redo  | bold italic underline strikeThrough subscript superscript outdent indent clearFormatting insertTable | insertImage insertVideo insertFile | html insertLink] } } }
       end
       f.input :main_image, as: :file
+      f.input :tags,
+        as: :select,
+        multiple: :true,
+        collection: ActsAsTaggableOn::Tag.select(:id, :name).all
     end
 
     f.actions
